5bd4c25589d5a7c9c36ac0241b9c4561c3b16273
pollobkumar1491 <pollobkumar1491@gmail.com>
2026-02-15 14:56:36 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+//firebase initialize korar jonno
+import 'package:firebase_core/firebase_core.dart';
+import 'login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
